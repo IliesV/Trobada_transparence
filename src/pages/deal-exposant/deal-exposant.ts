6e1425db6e9435fb68fe3851c724c81744783257
ThomasBeWeb <camot81@gmail.com>
2018-09-12 17:14:08 +0200

    essai

--- src/pages/deal-exposant/deal-exposant.ts
+++ src/pages/deal-exposant/deal-exposant.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {App} from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 import {LoginPage} from '../login/login';
 
@@ -22,7 +23,36 @@ export class DealExposantPage {
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private alertCtrl: AlertController,
-    private app: App) {
+    private app: App,
+    private qrScanner: QRScanner
+  ) {
+  }
+
+  private scanme(){
+    // Optionally request the permission early
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+      if (status.authorized) {
+        // camera permission was granted
+    
+    
+        // start scanning
+        let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+          console.log('Scanned something', text);
+    
+          this.qrScanner.hide(); // hide camera preview
+          scanSub.unsubscribe(); // stop scanning
+        });
+    
+      } else if (status.denied) {
+        // camera permission was permanently denied
+        // you must use QRScanner.openSettings() method to guide the user to the settings page
+        // then they can grant the permission from there
+      } else {
+        // permission was denied, but not permanently. You can ask for permission again at a later time.
+      }
+    })
+    .catch((e: any) => console.log('Error is', e));
   }
 
   private logout(){
