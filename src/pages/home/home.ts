a0dc2c3dda1d970d45acea375c8899a9c4c0770c
Unknown <ilies1512.viellard@yahoo.fr>
2018-09-12 15:25:19 +0200

    Ajout du bouton logout
    Bouton logout présent sur toutes les vues. Fonctionne correctement.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import {App} from 'ionic-angular';
+
+import {LoginPage} from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -7,9 +11,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private alertCtrl: AlertController,
+    private app: App) {
 
   }
-    
+
+  private logout(){
+    console.log("merde")
+    let alert = this.alertCtrl.create({
+      title: 'Confirmation',
+      message: 'Voulez vous vraiment vous déconnecter?',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Oui',
+          handler: () => {
+            this.app.getRootNav().setRoot(LoginPage);
+            console.log('Buy clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
 
 }
