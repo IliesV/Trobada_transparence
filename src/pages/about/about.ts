aa8f506e19aecc30c69f2e6fead9592369f9fbff
Unknown <camot81@gmail.com>
2018-09-20 14:47:43 +0200

    Recup boulot perdu

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {App} from 'ionic-angular';
 
-import {LoginPage} from '../login/login';
+import { DeconnexionProvider } from '../../providers/deconnexion/deconnexion';
 
 
 @Component({
@@ -14,33 +14,14 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController,
     private alertCtrl: AlertController,
-    private app: App) {
+    private app: App,
+    private deconnexionProvider: DeconnexionProvider
+    ) {
 
   }
 
   private logout(){
-    console.log("merde")
-    let alert = this.alertCtrl.create({
-      title: 'Confirmation',
-      message: 'Voulez vous vraiment vous déconnecter?',
-      buttons: [
-        {
-          text: 'Annuler',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Oui',
-          handler: () => {
-            this.app.getRootNav().setRoot(LoginPage);
-            console.log('Buy clicked');
-          }
-        }
-      ]
-    });
-    alert.present();
+   this.deconnexionProvider.logout();
   }
 
 }
