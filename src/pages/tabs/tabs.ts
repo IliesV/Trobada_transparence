8063df9ecd6568621c686e358440bca035a3f2e1
Tom <tcantinelli@gmail.com>
2018-09-27 23:26:44 +0200

    Trobaba color Home

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,7 +5,6 @@ import { AboutPage } from '../about/about';
 import { HomePage } from '../home/home';
 
 @Component({
-  selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
