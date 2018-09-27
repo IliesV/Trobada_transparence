98175e7f29e3befb18c95989db96e0a7021146f0
Tom <tcantinelli@gmail.com>
2018-09-27 21:15:58 +0200

    Color tab et title

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,6 +5,7 @@ import { AboutPage } from '../about/about';
 import { HomePage } from '../home/home';
 
 @Component({
+  selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
