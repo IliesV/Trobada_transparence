337a073040bd28ab031f983dfc8ed5b6fc4f7c9a
ThomasBeWeb <camot81@gmail.com>
2018-09-27 16:31:28 +0200

    Affichage qrCode

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,6 +6,8 @@ import {App} from 'ionic-angular';
 import {LoginPage} from '../login/login';
 import {ConnexionApiProvider} from '../../providers/api/api.connexion';
 
+import { NativeStorage } from '@ionic-native/native-storage';
+import { UserGlobal } from '../../models/infosUser.model';
 
 @Component({
   selector: 'page-about',
@@ -13,10 +15,14 @@ import {ConnexionApiProvider} from '../../providers/api/api.connexion';
 })
 export class AboutPage {
 
+  public myQrCode: string = null;
+  infosUser:UserGlobal = new UserGlobal();
+
   constructor(public navCtrl: NavController,
     private alertCtrl: AlertController,
     private app: App,
-    private connexionApiProvider: ConnexionApiProvider
+    private connexionApiProvider: ConnexionApiProvider,
+    private nativeStorage: NativeStorage
     ) {
 
   }
@@ -44,4 +50,13 @@ export class AboutPage {
     alert.present();
   }
 
+  ionViewCanEnter(){
+      //Recup Infos
+      this.nativeStorage.getItem('infosUser')
+      .then( infos => {
+        this.infosUser = infos as UserGlobal
+        this.myQrCode = '3-'+this.infosUser.pseudo
+      })
+      .catch(() => console.log('erreur recup infos'))
+  }
 }
