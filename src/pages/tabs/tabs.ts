29cd5788277ef6ca3f5639cc6c496add503759cf
Tom <tcantinelli@gmail.com>
2018-09-30 14:33:28 +0200

    clavier number code pin

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,7 @@ import { NavParams } from 'ionic-angular';
 import { QrcodePage } from '../qrcode/qrcode';
 import { HomePage } from '../home/home';
 import { TransactionsPage } from '../transactions/transactions';
-import { ValidationFestivalierPage } from '../validation-festivalier/validation-festivalier';
+//import { ValidationFestivalierPage } from '../validation-festivalier/validation-festivalier';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +13,7 @@ export class TabsPage {
 
   infosUser = {};
 
-  tab1Root = ValidationFestivalierPage;
+  tab1Root = HomePage;
   tab2Root = QrcodePage;
   tab3Root = TransactionsPage;
 
