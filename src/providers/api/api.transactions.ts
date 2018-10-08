2a90185f587fe3bd8d93815f343e86a983b9ac77
ThomasBeWeb <camot81@gmail.com>
2018-10-08 17:32:33 +0200

    Corrections Requetes

--- src/providers/api/api.transactions.ts
+++ src/providers/api/api.transactions.ts
@@ -9,6 +9,8 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class TransactionsApiProvider {
 
+    private baseUrl: string = 'http://trobada-api.fabrique-beweb.com/api';
+
     constructor(
         private http: HTTP,
         private transaction: TransactionProvider
@@ -17,7 +19,7 @@ export class TransactionsApiProvider {
     //Recup solde API
     public giveMySoldeOnline(token):Promise<HTTPResponse> {
 
-        const URL = 'http://trobadapi.ddns.info/api/solde'
+        const URL = this.baseUrl+'/solde'
         this.http.setDataSerializer('JSON');
         return this.http.get(URL, {}, {"Content-Type": "application/json","Authorization":"Bearer " + token})
             
@@ -26,7 +28,7 @@ export class TransactionsApiProvider {
     //Recup liste transaction
     public giveMyTransactions(token):Promise<HTTPResponse> {
 
-        const URL = 'http://trobadapi.ddns.info/api/transactions'
+        const URL = this.baseUrl+'/transactions'
         this.http.setDataSerializer('JSON');
         return this.http.get(URL, {}, {"Content-Type": "application/json","Authorization":"Bearer " + token})
             
@@ -35,7 +37,7 @@ export class TransactionsApiProvider {
     //Recup last transaction Vendeur
     public lastVendeurTransaction(token):Promise<HTTPResponse> {
 
-        const URL = 'http://trobadapi.ddns.info/api/lastTransacVendeur'
+        const URL = this.baseUrl+'/lastTransacVendeur'
         this.http.setDataSerializer('JSON');
         return this.http.get(URL, {}, {"Content-Type": "application/json","Authorization":"Bearer " + token})
             
@@ -44,7 +46,7 @@ export class TransactionsApiProvider {
     //Recup last transaction Client
     public lastClientTransaction(token):Promise<HTTPResponse> {
 
-        const URL = 'http://trobadapi.ddns.info/api/lastTransacClient'
+        const URL = this.baseUrl+'/lastTransacClient'
         this.http.setDataSerializer('JSON');
         return this.http.get(URL, {}, {"Content-Type": "application/json","Authorization":"Bearer " + token})
             
@@ -53,14 +55,14 @@ export class TransactionsApiProvider {
     //Envoi de la transaction.
     public sendTransactions(token, datas):Promise<HTTPResponse> {
 
-        const URL = 'http://trobadapi.ddns.info/api/addTransaction';
+        const URL = this.baseUrl+'/addTransaction';
         this.http.setDataSerializer('JSON');
         return this.http.post(URL, datas, {"Content-Type": "application/json", "Authorization":"Bearer " + token});
     }
 
     //CheckTransac from client
     public checkClient(idCom: string, pseudoCom: string, idTransac: number, montant: number, token:string): Promise<any> {
-        const URL = 'http://trobadapi.ddns.info/api/checkClient'
+        const URL = this.baseUrl+'/checkClient'
         this.http.setDataSerializer('JSON');
         return this.http.post(URL, {
             "id_com": idCom,
@@ -76,7 +78,7 @@ export class TransactionsApiProvider {
 
     //CheckTransac from vendeur
     public checkVendeur(idTransac:string, token:string): Promise<any> {
-        const URL = 'http://trobadapi.ddns.info/api/checkVendeur'
+        const URL = this.baseUrl+'/checkVendeur'
         this.http.setDataSerializer('JSON');
         return this.http.post(URL, {"idTransac": idTransac},
         {
@@ -87,7 +89,7 @@ export class TransactionsApiProvider {
 
     //validate transaction from vendeur
     public validateTransac(idTransac:string, token:string): Promise<any> {
-        const URL = 'http://trobadapi.ddns.info/api/validation'
+        const URL = this.baseUrl+'/validation'
         this.http.setDataSerializer('JSON');
         return this.http.post(URL, {"idTransac": idTransac},
         {
